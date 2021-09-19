9d88532fd0574e56a478c88e29e3ab8e7e5fcd77
JOEL EYONG <62267332+eyong-joel7@users.noreply.github.com>
2021-09-19 22:32:28 +0300

    Added functionality to send private message

--- index.js
+++ index.js
@@ -15,41 +15,48 @@ const io = require("socket.io")(server, {
   },
 });
 
-
 const PORT = process.env.PORT || 5000;
 
 const users = {};
-function capitalize(s)
-{
-    return s && s[0].toUpperCase() + s.slice(1);
+function capitalize(s) {
+  return s && s[0].toUpperCase() + s.slice(1);
 }
 const socketToRoom = {};
 
 io.on("connection", (socket) => {
-  socket.on("join room", ({ roomID, userName:name }, callBack) => {
-    const { error, user } = addUser({ id: socket.id, name, roomID, isAdmin:false });
+  socket.on("join room", ({ roomID, userName: name }, callBack) => {
+    const { error, user } = addUser({
+      id: socket.id,
+      name,
+      roomID,
+      isAdmin: false,
+    });
 
     if (error) return callBack(error);
     if (users[roomID]) {
       const length = users[roomID].length;
       if (length === 4) {
         removeUser(socket.id);
-        return callBack('Sorry, this room is full. Please Contact the organizer')
+        return callBack(
+          "Sorry, this room is full. Please Contact the organizer"
+        );
       }
       const newUser = {
-       id: socket.id,
-       userName:name,
-      }
+        id: socket.id,
+        userName: name,
+      };
       users[roomID].push(newUser);
       socket.username = name;
     } else {
       // users[roomID] = [socket.id];
-      return callBack('The meeting might have not started or meeting ID is incorrect')
+      return callBack(
+        "The meeting might have not started or meeting ID is incorrect"
+      );
     }
 
     //messaging implementaion
-    
-    socket.join(user.room)
+
+    socket.join(user.room);
     const userName = capitalize(name);
     io.to(socket.id).emit("message", {
       user: "admin",
@@ -65,22 +72,28 @@ io.on("connection", (socket) => {
     });
 
     socketToRoom[socket.id] = roomID;
-    const usersInThisRoom = users[roomID].filter((user) => user.id !== socket.id);
+    const usersInThisRoom = users[roomID].filter(
+      (user) => user.id !== socket.id
+    );
     socket.emit("all users", usersInThisRoom);
-    console.log('usersInThisRoom',usersInThisRoom)
+    console.log("usersInThisRoom", usersInThisRoom);
   });
 
-  socket.on("start a meeting", ({ roomID, userName:name }, callBack) => {
-    const { error, user } = addUser({ id: socket.id, name, roomID, isAdmin:true });
+  socket.on("start a meeting", ({ roomID, userName: name }, callBack) => {
+    const { error, user } = addUser({
+      id: socket.id,
+      name,
+      roomID,
+      isAdmin: true,
+    });
     if (error) return callBack(error);
     const newUser = {
       id: socket.id,
-      userName:name,
-     }
+      userName: name,
+    };
     if (roomID in users) {
-       users[roomID].push(newUser);
-    }
-    else {
+      users[roomID].push(newUser);
+    } else {
       users[roomID] = [newUser];
     }
     socket.username = name;
@@ -100,24 +113,26 @@ io.on("connection", (socket) => {
     });
 
     socketToRoom[socket.id] = roomID;
-    const usersInThisRoom = users[roomID].filter((user) =>user.id !== socket.id);
+    const usersInThisRoom = users[roomID].filter(
+      (user) => user.id !== socket.id
+    );
     socket.emit("all users", usersInThisRoom);
-    console.log('usersInThisRoom',usersInThisRoom)
+    console.log("usersInThisRoom", usersInThisRoom);
   });
 
-
-
-  socket.on("sendMessage", ({message, userID}, callback) => {
+  socket.on("sendMessage", ({ message, userID, recipient }, callback) => {
     const user = getUser(socket.id);
-  user &&  io.to(user.room).emit("message", { user: user.name, text: message });
+    user && recipient
+      ? io.to(recipient).to(socket.id).emit("message", { user: user.name, text: message })
+      : io.to(user.room).emit("message", { user: user.name, text: message });
     callback();
   });
 
   socket.on("sending signal", (payload) => {
-    io.to(payload.userToSignal).emit('user joined', {
+    io.to(payload.userToSignal).emit("user joined", {
       signal: payload.signal,
       callerID: payload.callerID,
-      userName: socket.username
+      userName: socket.username,
     });
   });
 
@@ -145,7 +160,7 @@ io.on("connection", (socket) => {
     const roomID = socketToRoom[socket.id];
     const user = removeUser(socket.id);
     if (user) {
-      const userName = capitalize( user.name);
+      const userName = capitalize(user.name);
       io.to(roomID).emit("message", {
         user: "Admin",
         text: `${userName} has left.`,
@@ -161,19 +176,17 @@ io.on("connection", (socket) => {
       room = room.filter((user) => user.id !== socket.id);
       users[roomID] = room;
     }
-    socket.broadcast.to(roomID).emit('user left', socket.id)
+    socket.broadcast.to(roomID).emit("user left", socket.id);
   });
-  
-  socket.on('change', (payload) => {
-    socket.broadcast.emit('change',payload)
-});
-  socket.on('admin playstop muteunmute', (payload) => {
-    socket.broadcast.emit('admin playstop muteunmute', payload)
-});
 
+  socket.on("change", (payload) => {
+    socket.broadcast.emit("change", payload);
+  });
+  socket.on("admin playstop muteunmute", (payload) => {
+    io.to(payload.peerID).emit("admin playstop muteunmute", payload);
+  });
 });
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
